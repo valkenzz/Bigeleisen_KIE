786a3c11ebd4842b08d3524d5efac03e09e2a706
valkenzz <40594333+valkenzz@users.noreply.github.com>
2021-06-25 21:11:46 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='Bigeleisen_KIE',
+      version='1.0',
+      py_modules=['Bigeleisen_KIE'],
+      description='library which provides tools capable of calculating the Kinetic Isotope Effect according to the Bigeleisen equation',
+      author='Valkenzz',
+      author_email='valentin.meo.1@ulaval.ca',
+      url='https://github.com/valkenzz/Bigeleisen_KIE',
+      )
